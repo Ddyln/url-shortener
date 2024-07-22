9edc5312f96914bf56993d83bba2350642aba73c
Dang Duy Lan <ddlan99tn@gmail.com>
2024-07-22 23:44:22 +0700

    test connection with db, add first document

--- connect.js
+++ connect.js
@@ -3,6 +3,21 @@ const connectToDatabase = async () => {
     var mongoose = require('mongoose');
     //Lets connect to our database using the DB server URL.
     mongoose.connect('mongodb+srv://user:abcd1234@cluster-mongo-test.ueql9iv.mongodb.net/url-shortener?retryWrites=true&w=majority&appName=cluster-mongo-test')
-        .then((result) => {return mongoose;})
+        .then((result) => {
+            console.log('Connected');
+            var URL = mongoose.model('link', {
+                ori: String,
+                new: String,
+                lastActive: Date
+            });
+            URL.create({
+                ori: 'localhost:5500',
+                new: 'localhost:5500',
+                lastActive: new Date().toUTCString()
+            })
+                .then(res => console.log(res)); 
+        })
         .catch(error => {throw error;});
 }
+
+connectToDatabase();
